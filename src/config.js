40425877c5a52f11ec6cdc2d6d53b11d0cd7e176
Sravan Shanker <iamsravan.jr@gmail.com>
2025-12-18 17:18:09 +0530

    added ability to change flock number on the fly

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
   boidCount: 240,
-  flockCount: 5,
+  flockCount: 3,
   maxSpeed: 3.5,
   maxForce: 0.1,
   visualRange: 60,
@@ -11,6 +11,15 @@ const CONFIG = {
   wanderWeight: 1.0,
 
   wanderAngle: Math.PI / 3,
+
+  colors: [
+    "#f38ba8",
+    "#a6e3a1",
+    "#89b4fa",
+    "#fddd6bff",
+    "#df68fdff",
+    "#ffffffff",
+  ],
 
   trailLength: 12,
   trailEnabled: true,
